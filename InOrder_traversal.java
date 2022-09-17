6abfe26315638e8a7bdbf3425988b6b5b0735b7b
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-09-17 19:04:40 +0530

    Create InOrder_traversal.java

---
+++ InOrder_traversal.java
@@ -0,0 +1,39 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		root.left=new Node(20);
+		root.left.left=new Node(30);
+		root.left.right=new Node(40);
+		root.right=new Node(50);
+		root.right.left=new Node(60);
+		root.right.right=new Node(70);
+		inOrder(root);
+		}
+// 		inOrder traverse:- left root right
+//   		10               
+//        /   \
+//      20     50
+//    /   \   /  \
+//  30    40 60   70
+//  inorder : 30 20 40 10 60 50 70
+
+	static void inOrder(Node root)	{
+	    if(root!=null){
+	        inOrder(root.left);
+	        System.out.print(root.key+" ");
+	        inOrder(root.right);
+	       
+	    }
+	}
+}
+class Node {
+    Node root ;
+    int key;
+    Node left;
+    Node right;
+    Node(int k){
+        key= k;
+        right=left=null;
+    }
+}
